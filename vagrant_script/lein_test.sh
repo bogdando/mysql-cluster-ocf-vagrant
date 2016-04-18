455861f88630a2e33f45788e0f18b55cbf1c3b30
Bogdan Dobrelya <bdobrelia@mirantis.com>
2016-04-18 16:41:01 +0200

    Fix jepsen test runner
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- vagrant_script/lein_test.sh
+++ vagrant_script/lein_test.sh
@@ -18,6 +18,16 @@ then
   docker pull pandeiro/lein
 fi
 
+# Run lein to make a custom galera dependency build
+docker stop jepsen && docker rm -f -v jepsen
+echo "Make a custom galera jar build"
+docker run -it --rm \
+  -v /jepsen/jepsen/galera:/app \
+  --entrypoint /bin/bash \
+  --name jepsen -h jepsen \
+  pandeiro/lein:latest -c "lein deps && lein compile && lein uberjar; sync"
+sync
+
 # FIXME(bogdando) remove those customs, when build is not required anymore
 # Run lein to make a custom jepsen build
 docker stop jepsen && docker rm -f -v jepsen
@@ -37,10 +47,13 @@ docker run --stop-signal=SIGKILL -itd \
   -v /root/.ssh:/root/.ssh:ro \
   -v /jepsen/jepsen/$1:/app \
   -v /jepsen/jepsen/jepsen/target:/custom:ro \
+  -v /jepsen/jepsen/galera/target:/custom2:ro \
   -v /jepsen/logs:/app/store \
   --entrypoint /bin/bash \
   --name jepsen -h jepsen \
   pandeiro/lein:latest
+docker exec -it jepsen bash -c "mkdir -p resources/jepsen/galera/0.1.0-SNAPSHOT"
+docker exec -it jepsen bash -c "cp -f /custom2/jepsen.galera-0.1.0-SNAPSHOT*  resources/jepsen/galera/0.1.0-SNAPSHOT/"
 docker exec -it jepsen bash -c "mkdir -p resources/jepsen/jepsen/0.1.0-SNAPSHOT"
 docker exec -it jepsen bash -c "cp -f /custom/jepsen-0.1.0-SNAPSHOT*  resources/jepsen/jepsen/0.1.0-SNAPSHOT/"
 docker exec -it jepsen bash -c "lein deps && lein compile && lein test"
