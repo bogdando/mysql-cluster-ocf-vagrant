ef4eaf448d8b12bf4711834012f8950bbf3e7beb
Bogdan Dobrelya <bdobrelia@mirantis.com>
2016-04-21 13:02:25 +0200

    Rework purging for lein test/build containers
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- vagrant_script/lein_test.sh
+++ vagrant_script/lein_test.sh
@@ -21,54 +21,54 @@ fi
 
 # Try to start the jepsen container w/o purging it
 docker start jepsen || PURGE=true
+
+# Make custom builds only when purging as well
 if [ "${PURGE}" = "true" ]; then
   docker stop jepsen && docker rm -f -v jepsen
-fi
+  # Run lein to make a custom galera dependency build
+  docker stop jepsen-build && docker rm -f -v jepsen-build
+  echo "Make a custom galera jar build"
+  docker run -it --rm \
+    -v /jepsen/jepsen/galera:/app \
+    --entrypoint /bin/bash \
+    --name jepsen-build -h jepsen \
+    pandeiro/lein:latest -c "lein deps && lein compile && lein uberjar; sync"
+  sync
 
-# Run lein to make a custom galera dependency build
-docker stop jepsen-build && docker rm -f -v jepsen-build
-echo "Make a custom galera jar build"
-docker run -it --rm \
-  -v /jepsen/jepsen/galera:/app \
-  --entrypoint /bin/bash \
-  --name jepsen-build -h jepsen \
-  pandeiro/lein:latest -c "lein deps && lein compile && lein uberjar; sync"
-sync
+  # FIXME(bogdando) remove those customs, when build is not required anymore
+  # Run lein to make a custom jepsen build
+  docker stop jepsen-build && docker rm -f -v jepsen-build
+  echo "Make a custom jepson jar build"
+  docker run -it --rm \
+    -v /jepsen/jepsen/jepsen:/app \
+    --entrypoint /bin/bash \
+    --name jepsen-build -h jepsen \
+    pandeiro/lein:latest -c "lein deps && lein compile && lein uberjar; sync"
+  sync
 
-# FIXME(bogdando) remove those customs, when build is not required anymore
-# Run lein to make a custom jepsen build
-docker stop jepsen-build && docker rm -f -v jepsen-build
-echo "Make a custom jepson jar build"
-docker run -it --rm \
-  -v /jepsen/jepsen/jepsen:/app \
-  --entrypoint /bin/bash \
-  --name jepsen-build -h jepsen \
-  pandeiro/lein:latest -c "lein deps && lein compile && lein uberjar; sync"
-sync
+  # Run lein for jepsen tests, using the custom build from the target dir mounted
+  # Ignore exit code as it may fail. Distributed systems are faily with jepsen...
+  echo "Run lein test"
+  docker run --stop-signal=SIGKILL -itd \
+    -v /etc/hosts:/etc/hosts:ro \
+    -v /root/.ssh:/root/.ssh:ro \
+    -v /jepsen/jepsen/$1:/app \
+    -v /jepsen/jepsen/jepsen/target:/custom:ro \
+    -v /jepsen/jepsen/galera/target:/custom2:ro \
+    -v /jepsen/logs:/app/store \
+    --entrypoint /bin/bash \
+    --name jepsen -h jepsen \
+    pandeiro/lein:latest
 
-# Run lein for jepsen tests, using the custom build from the target dir mounted
-# Ignore exit code as it may fail. Distributed systems are faily with jepsen...
-echo "Run lein test"
-docker run --stop-signal=SIGKILL -itd \
-  -v /etc/hosts:/etc/hosts:ro \
-  -v /root/.ssh:/root/.ssh:ro \
-  -v /jepsen/jepsen/$1:/app \
-  -v /jepsen/jepsen/jepsen/target:/custom:ro \
-  -v /jepsen/jepsen/galera/target:/custom2:ro \
-  -v /jepsen/logs:/app/store \
-  --entrypoint /bin/bash \
-  --name jepsen -h jepsen \
-  pandeiro/lein:latest
-if [ "${PURGE}" = "true" ]; then
   # install dependency
-  docker exec -it jepsen bash -c "apt-get update"
-  docker exec -it jepsen bash -c "apt-get -y install gnuplot-qt"
+    docker exec -it jepsen bash -c "apt-get update"
+    docker exec -it jepsen bash -c "apt-get -y install gnuplot-qt"
+  # copy custom jar builds
+  docker exec -it jepsen bash -c "mkdir -p resources/jepsen/galera/0.1.0-SNAPSHOT"
+  docker exec -it jepsen bash -c "cp -f /custom2/jepsen.galera-0.1.0-SNAPSHOT*  resources/jepsen/galera/0.1.0-SNAPSHOT/"
+  docker exec -it jepsen bash -c "mkdir -p resources/jepsen/jepsen/0.1.0-SNAPSHOT"
+  docker exec -it jepsen bash -c "cp -f /custom/jepsen-0.1.0-SNAPSHOT*  resources/jepsen/jepsen/0.1.0-SNAPSHOT/"
 fi
-# copy custom jar builds
-docker exec -it jepsen bash -c "mkdir -p resources/jepsen/galera/0.1.0-SNAPSHOT"
-docker exec -it jepsen bash -c "cp -f /custom2/jepsen.galera-0.1.0-SNAPSHOT*  resources/jepsen/galera/0.1.0-SNAPSHOT/"
-docker exec -it jepsen bash -c "mkdir -p resources/jepsen/jepsen/0.1.0-SNAPSHOT"
-docker exec -it jepsen bash -c "cp -f /custom/jepsen-0.1.0-SNAPSHOT*  resources/jepsen/jepsen/0.1.0-SNAPSHOT/"
 
 testcase="lein test"
 [ "${2}" ] && testcase="${testcase} :only jepsen.${1}-test/${2}"
